dcaebf0fe6d00d84df414e466e25ebe45d04ff15
mmakos <mmakos.pl@gmail.com>
2023-11-17 00:01:36 +0100

    Done year 2022 day 14 and started 15 (todo change char array to range accumulator)

--- src/main/java/pl/mmakos/advent/year2022/Day14.java
+++ src/main/java/pl/mmakos/advent/year2022/Day14.java
@@ -2,6 +2,12 @@ package pl.mmakos.advent.year2022;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import pl.mmakos.advent.utils.Point;
+import pl.mmakos.advent.utils.Utils;
+
+import java.util.Arrays;
+
+import static java.util.function.Predicate.not;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public final class Day14 {
@@ -12,10 +18,116 @@ public final class Day14 {
   }
 
   private static int task1() {
-    return 0;
+    char[][] cave = getCave();
+    int dropped = dropSand(cave);
+    for (char[] row : cave) {
+      System.err.println(new String(row));
+    }
+    return dropped;
   }
 
   private static int task2() {
-    return 0;
+    char[][] cave = getCave();
+    char[][] newCave = new char[cave.length + 2][];
+    System.arraycopy(cave, 0, newCave, 0, cave.length);
+    newCave[cave.length] = new char[cave[0].length];
+    newCave[cave.length + 1] = new char[cave[0].length];
+    Arrays.fill(newCave[cave.length], '.');
+    Arrays.fill(newCave[cave.length + 1], '#');
+    int dropped = dropSand(newCave);
+    for (char[] row : newCave) {
+      System.err.println(new String(row));
+    }
+    return dropped;
+  }
+
+  private static int dropSand(char[][] cave) {
+    int startX = new String(cave[0]).indexOf('+');
+
+    int drops = 0;
+    while (true) {
+      int x = startX, y = 0;
+      while (true) {
+        if (cave[y + 1][x] == '.') {
+          ++y;
+        } else if (cave[y + 1][x - 1] == '.') {
+          --x;
+          ++y;
+        } else if (cave[y + 1][x + 1] == '.') {
+          ++x;
+          ++y;
+        } else {
+          cave[y][x] = 'o';
+          if (y == 0) {
+            return drops + 1;
+          }
+          break;
+        }
+        if (y >= cave.length - 1) {
+          return drops;
+        }
+      }
+
+      ++drops;
+    }
+  }
+
+  private static char[][] getCave() {
+    Point[][] paths = paths();
+    int maxY = Arrays.stream(paths)
+            .flatMap(Arrays::stream)
+            .mapToInt(Point::y)
+            .max()
+            .orElseThrow();
+    // safe bias
+    int minX = 500 - maxY - 2;
+    int maxX = 500 + maxY + 2;
+    int startX = 500 - minX;
+
+    char[][] cave = new char[maxY + 1][maxX - minX + 1];
+    for (char[] chars : cave) {
+      Arrays.fill(chars, '.');
+    }
+
+    for (Point[] path : paths) {
+      Point start = path[0];
+      cave[start.y()][start.x() - minX] = '#';
+      for (int i = 1; i < path.length; ++i) {
+        Point end = path[i];
+        if (start.x() < end.x()) {
+          for (int x = start.x() - minX + 1; x <= end.x() - minX; ++x) {
+            cave[start.y()][x] = '#';
+          }
+        } else if (start.x() > end.x()) {
+          for (int x = start.x() - minX - 1; x >= end.x() - minX; --x) {
+            cave[start.y()][x] = '#';
+          }
+        } else if (start.y() < end.y()) {
+          for (int y = start.y() + 1; y <= end.y(); ++y) {
+            cave[y][start.x() - minX] = '#';
+          }
+        } else if (start.y() > end.y()) {
+          for (int y = start.y() - 1; y >= end.y(); --y) {
+            cave[y][start.x() - minX] = '#';
+          }
+        }
+
+        start = end;
+      }
+    }
+
+    cave[0][startX] = '+';
+
+    return cave;
+  }
+
+  private static Point[][] paths() {
+    return Utils.lines(14, 2022)
+            .filter(not(String::isBlank))
+            .map(s -> Arrays.stream(s.split(" -> "))
+                    .map(str -> str.split(","))
+                    .map(str -> new Point(Integer.parseInt(str[0]), Integer.parseInt(str[1])))
+                    .toArray(Point[]::new))
+            .toArray(Point[][]::new);
   }
 }
--- src/main/java/pl/mmakos/advent/year2022/Day15.java
+++ src/main/java/pl/mmakos/advent/year2022/Day15.java
@@ -2,9 +2,28 @@ package pl.mmakos.advent.year2022;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import pl.mmakos.advent.utils.Pair;
+import pl.mmakos.advent.utils.Point;
+import pl.mmakos.advent.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.abs;
+import static java.util.function.Predicate.not;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public final class Day15 {
+  private static final Pattern INPUT_PATTERN = Pattern.compile("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+
   @SuppressWarnings("java:S106")
   public static void main(String[] args) {
     System.err.println("TASK 1: " + task1());
@@ -12,10 +31,109 @@ public final class Day15 {
   }
 
   private static int task1() {
-    return 0;
+    Pair<Point, Point>[] input = input();
+    int[] bounds = getBounds(input);
+    int row = 2_000_000;
+    char[] rowChars = new char[bounds[1] - bounds[0] + 1];
+    int zeroPos = -bounds[0];
+
+    return (int) find(row, zeroPos, rowChars, input).chars()
+            .filter(c -> c == '#')
+            .count();
   }
 
   private static int task2() {
+    Pair<Point, Point>[] input = input();
+    AtomicInteger result = new AtomicInteger();
+
+    ExecutorService executorService = Executors.newFixedThreadPool(16);
+    for (int i = 0; i < 16; ++ i) {
+      int finalI = i * 250_000;
+      char[] rowChars = new char[4_000_000];
+      executorService.execute(() -> {
+        for (int row = finalI; row < finalI + 250_000; ++row) {
+          String str = find(row, 0, rowChars, input);
+          int idx = str.indexOf('\0');
+          Arrays.fill(rowChars, '\0');
+          if (idx != -1) {
+            result.set(4_000_000 * idx + row);
+            System.err.println("TASK 2!!!: " + result.get());
+            executorService.shutdown();
+            return;
+          }
+          if (row % 10_000 == 0) {
+            System.err.println(row);
+          }
+        }
+      });
+    }
+
     return 0;
+  }
+
+  private static String find(int row, int zeroPos, char[] rowChars, Pair<Point, Point>[] input) {
+    for (Pair<Point, Point> pair : input) {
+      Point sensor = pair.first();
+      Point beacon = pair.second();
+
+      if (beacon.y() == row && beacon.x() >= -zeroPos) {
+        rowChars[beacon.x() + zeroPos] = 'B';
+      }
+      int noBeaconFieldsInR = distance(sensor, beacon) - abs(sensor.y() - row);
+      if (noBeaconFieldsInR >= 0) {
+        for (int i = Math.max(-zeroPos, sensor.x() - noBeaconFieldsInR + zeroPos); i < Math.min(rowChars.length + zeroPos, sensor.x() + noBeaconFieldsInR + zeroPos + 1); ++i) {
+          if (rowChars[i] != 'B') {
+            rowChars[i] = '#';
+          }
+        }
+      }
+      if (!new String(rowChars).contains("\0")) break;
+    }
+
+    return new String(rowChars);
+  }
+
+  @SuppressWarnings("unchecked")
+  private static int[] getBounds(Pair<Point, Point>[] input) {
+    List<Integer>[] boundLists = new List[]{new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>()};
+    Arrays.stream(input)
+            .map(p -> getBounds(p.first(), p.second()))
+            .forEach(b -> {
+              boundLists[0].add(b[0]);
+              boundLists[1].add(b[1]);
+              boundLists[2].add(b[2]);
+              boundLists[3].add(b[3]);
+            });
+    return new int[]{
+            boundLists[0].stream().mapToInt(i -> i).min().orElseThrow(),
+            boundLists[1].stream().mapToInt(i -> i).max().orElseThrow(),
+            boundLists[2].stream().mapToInt(i -> i).min().orElseThrow(),
+            boundLists[3].stream().mapToInt(i -> i).max().orElseThrow(),
+    };
+  }
+
+  @SuppressWarnings("unchecked")
+  private static Pair<Point, Point>[] input() {
+    return Utils.lines(15, 2022)
+            .filter(not(String::isBlank))
+            .map(INPUT_PATTERN::matcher)
+            .filter(Matcher::matches)
+            .map(m -> new Pair<>(new Point(parseInt(m.group(1)), parseInt(m.group(2))),
+                    new Point(parseInt(m.group(3)), parseInt(m.group(4)))))
+            .toArray(Pair[]::new);
+  }
+
+  private static int distance(Point sensor, Point beacon) {
+    return abs(sensor.x() - beacon.x()) + abs(sensor.y() - beacon.y());
+  }
+
+  private static int[] getBounds(Point sensor, Point beacon) {
+    int distance = distance(sensor, beacon);
+    return new int[]{
+            sensor.x() - distance,  // min x
+            sensor.x() + distance,  // max x
+            sensor.y() - distance,  // min y
+            sensor.y() + distance,  // max y
+    };
   }
 }
