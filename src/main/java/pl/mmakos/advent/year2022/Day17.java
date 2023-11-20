224f8e4a2ee805aebe7e42c58ffebb674eb2013a
Michał Makoś <mmakos@merlin.pho>
2023-11-20 14:16:33 +0100

    Added year 2022 day 17

--- src/main/java/pl/mmakos/advent/year2022/Day17.java
+++ src/main/java/pl/mmakos/advent/year2022/Day17.java
@@ -5,9 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import pl.mmakos.advent.utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 import static java.lang.Math.max;
@@ -21,20 +19,16 @@ public final class Day17 {
   }
 
   private static int task1() {
-    Iterator<Character> input = input();
+    char[] input = input();
+    Iterator<Character> inputIt = iterator(input());
     Iterator<Stone> stones = Stone.iterator();
-    Chamber chamber = new Chamber();
+    Chamber chamber = new Chamber(input.length);
 
     for (int i = 0; i < 2022; ++i) {
       chamber.addStone(stones.next());
-      while (true) {
-        chamber.moveHorizontally(input.next());
-        if (chamber.canMoveDown()) {
-          chamber.moveDown();
-        } else {
-          break;
-        }
-      }
+      do {
+        chamber.moveHorizontally(inputIt.next());
+      } while (chamber.moveDownIfPossible() == null);
     }
 
     System.err.println(chamber);
@@ -42,29 +36,60 @@ public final class Day17 {
     return chamber.height;
   }
 
-  private static int task2() {
-    return 0;
+  @SuppressWarnings("java:S127")
+  private static long task2() {
+    char[] input = input();
+    Iterator<Character> inputIt = iterator(input());
+    Iterator<Stone> stones = Stone.iterator();
+    Chamber chamber = new Chamber(input.length);
+    chamber.repetitionsEnabled = true;
+
+    long repetitionHeights = 0;
+    int[] repetition = new int[0];
+    for (long i = 0; i < 1_000_000_000_000L; ++i) {
+      if (repetition.length == 2) { // repetition occured
+        long height = repetition[0];
+        long length = repetition[1];
+
+        long repetitionsCount = (1_000_000_000_000L - i) / length;
+        repetitionHeights = height * repetitionsCount;
+        i += repetitionsCount * length;
+
+        chamber.repetitionsEnabled = false;
+      }
+
+      chamber.addStone(stones.next());
+      do {
+        chamber.moveHorizontally(inputIt.next());
+      } while ((repetition = chamber.moveDownIfPossible()) == null);
+    }
+
+    return chamber.height + repetitionHeights;
   }
 
-  private static Iterator<Character> input() {
-    char[] chars = Utils.read(17, 2022)
-            .toCharArray();
+  private static Iterator<Character> iterator(char[] chars) {
     return IntStream.iterate(0, i -> ++i % chars.length)
             .mapToObj(i -> chars[i])
             .iterator();
   }
 
+  private static char[] input() {
+    return Utils.read(17, 2022)
+            .toCharArray();
+  }
+
   @RequiredArgsConstructor
   private static class Stone {
-    private static final Stone hLine = new Stone(new byte[]{0b1111}, 4);
-    private static final Stone plus = new Stone(new byte[]{0b010, 0b111, 0b010}, 3);
-    private static final Stone lStone = new Stone(new byte[]{0b111, 0b001, 0b001}, 3);
-    private static final Stone vLine = new Stone(new byte[]{1, 1, 1, 1}, 1);
-    private static final Stone square = new Stone(new byte[]{0b11, 0b11}, 2);
+    private static final Stone hLine = new Stone(new byte[]{0b1111}, 4, 1);
+    private static final Stone plus = new Stone(new byte[]{0b010, 0b111, 0b010}, 3, 2);
+    private static final Stone lStone = new Stone(new byte[]{0b111, 0b001, 0b001}, 3, 3);
+    private static final Stone vLine = new Stone(new byte[]{1, 1, 1, 1}, 1, 4);
+    private static final Stone square = new Stone(new byte[]{0b11, 0b11}, 2, 5);
 
     // bottom to top
     private final byte[] bytes;
     private final int width;
+    private final int hashCode;
 
     private static Iterator<Stone> iterator() {
       Stone[] stones = new Stone[]{hLine, plus, lStone, vLine, square};
@@ -86,14 +111,31 @@ public final class Day17 {
       }
       return result;
     }
+
+    @Override
+    @SuppressWarnings("java:S1206")
+    public int hashCode() {
+      return hashCode;
+    }
   }
 
+  @RequiredArgsConstructor
   private static class Chamber {
     private final List<Byte> rows = new ArrayList<>();
+    private final int maxMovements;
     private int height;
     private Stone currentStone;
     private int currentStoneX;
     private int currentStoneY;
+
+    // For repetitions
+    // Every time the stone is dropped, last couple of rows are remembered as RepetitionKey with some info
+    // If this RepetitionKey occured again, then the difference between heights of stored repetition and current
+    // can be multiplied without actual dropping stones
+    private final Map<RepetitionKey, RepetitionValue> repetitionCandidates = new HashMap<>();
+    private boolean repetitionsEnabled;
+    private int movementIdx;
+    private int stones;
 
     private void addStone(Stone stone) {
       currentStone = stone;
@@ -111,30 +153,59 @@ public final class Day17 {
         case '>' -> moveRight();
         default -> throw new IllegalStateException();
       }
+      ++movementIdx;
+      movementIdx %= maxMovements;
     }
 
-    private void applyStone() {
+    private int[] applyStone() {
       for (int i = 0; i < currentStone.height(); ++i) {
         byte row = rows.get(currentStoneY + i);
         byte stone = (byte) (currentStone.bytes[i] << 7 - currentStone.width - currentStoneX);
         rows.set(currentStoneY + i, (byte) (row | stone));
       }
       height = max(height, currentStoneY + currentStone.height());
+      ++stones;
+
+      if (repetitionsEnabled) {
+        return proceedRepetition();
+      } else return new int[0];
     }
 
-    private boolean canMoveDown() {
+    // If this fragment is repeated, returns array of (stonesDiff, heightDiff)
+    // stonesDiff - how many rocks are repeated
+    // heightDiff - how many height is gained during repetition
+    private int[] proceedRepetition() {
+      // We don't count repetitions, if min length is not exceeded
+      if (height < RepetitionKey.DEFAULT_LENGTH) return new int[0];
+
+      byte[] lastNRows = bytes((height - RepetitionKey.DEFAULT_LENGTH), height);
+      RepetitionKey repetitionKey = new RepetitionKey(lastNRows, currentStone.hashCode, movementIdx);
+
+      RepetitionValue existing = repetitionCandidates.get(repetitionKey);
+      if (existing != null) {
+        int heightDiff = this.height - existing.height;
+        int stonesDiff = this.stones - existing.stones;
+
+        return new int[]{heightDiff, stonesDiff};
+      } else {
+        RepetitionValue repetitionValue = new RepetitionValue(height, stones);
+        repetitionCandidates.put(repetitionKey, repetitionValue);
+      }
+      return new int[0];
+    }
+
+    // if move possible then moves down and returns null
+    // if not then applies stone and returns repetition (if possible) or empty array
+    @SuppressWarnings("java:S1168")
+    private int[] moveDownIfPossible() {
       int mask = mask(currentStoneX, currentStoneY - 1);
       int stoneMask = currentStone.mask();
       if ((mask & stoneMask) > 0) {
-        applyStone();
-        return false;
+        return applyStone();
       } else {
-        return true;
+        --currentStoneY;
+        return null;
       }
-    }
-
-    private void moveDown() {
-      --currentStoneY;
     }
 
     private void moveLeft() {
@@ -166,6 +237,15 @@ public final class Day17 {
       return result;
     }
 
+    private byte[] bytes(int start, int end) {
+      List<Byte> sublist = rows.subList(start, end);
+      byte[] bytes = new byte[sublist.size()];
+      for (int i = 0; i < bytes.length; ++i) {
+        bytes[i] = sublist.get(i);
+      }
+      return bytes;
+    }
+
     @Override
     public String toString() {
       StringBuilder sb = new StringBuilder();
@@ -178,5 +258,49 @@ public final class Day17 {
       }
       return sb.toString();
     }
+  }
+
+  // Each time the stone is dropped, last <DEFAULT_LENGTH> rows are remembered in hash map
+  // Where key is RepetitionKey - identifies the same pattern throughout <DEFAULT_LENGTH> last rows
+  private static class RepetitionKey {
+    private static final int DEFAULT_LENGTH = 100;
+
+    private final byte[] bytes;
+    private final int stone;
+    private final int inputIdx;
+    private final int hashCode;
+
+    private RepetitionKey(byte[] bytes, int stone, int inputIdx) {
+      this.bytes = bytes;
+      this.stone = stone;
+      this.inputIdx = inputIdx;
+      hashCode = calcHash();
+    }
+
+    @Override
+    public int hashCode() {
+      return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      return obj instanceof RepetitionKey key &&
+              key.hashCode == hashCode &&
+              key.inputIdx == inputIdx &&
+              key.stone == stone &&
+              Arrays.equals(bytes, key.bytes);
+    }
+
+    private int calcHash() {
+      int hash = Arrays.hashCode(bytes);
+      hash *= 31;
+      hash += stone;
+      hash *= 31;
+      hash += inputIdx;
+      return hash;
+    }
+  }
+
+  private record RepetitionValue(int height, int stones) {
   }
 }
