0b2bcd63f62748d83c866d2337c6c698c2de97bb
Michał Makoś <mmakos.pl@gmail.com>
2023-12-08 17:21:47 +0100

    Added year 2021 day 7

--- src/main/java/pl/mmakos/advent/year2021/Day7.java
+++ src/main/java/pl/mmakos/advent/year2021/Day7.java
@@ -2,6 +2,12 @@ package pl.mmakos.advent.year2021;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import pl.mmakos.advent.utils.Utils;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.abs;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public final class Day7 {
@@ -12,10 +18,38 @@ public final class Day7 {
   }
 
   private static int task1() {
-    return 0;
+    int[] input = Utils.ints(7, 2021, ",").toArray();
+    int max = Arrays.stream(input).max().orElseThrow();
+
+    return IntStream.range(0, max)
+            .map(pos -> fuel(input, pos))
+            .min()
+            .orElseThrow();
   }
 
   private static int task2() {
-    return 0;
+    int[] input = Utils.ints(7, 2021, ",").toArray();
+    int max = Arrays.stream(input).max().orElseThrow();
+
+    return IntStream.range(0, max)
+            .map(pos -> fuel2(input, pos))
+            .min()
+            .orElseThrow();
+  }
+
+  private static int fuel(int[] positions, int pos) {
+    return Arrays.stream(positions)
+            .map(p -> abs(pos - p))
+            .sum();
+  }
+
+  private static int fuel2(int[] positions, int pos) {
+    return Arrays.stream(positions)
+            .map(p -> sumFrom1(abs(pos - p)))
+            .sum();
+  }
+
+  private static int sumFrom1(int val) {
+    return (int) ((val + 1) * (val / 2.));
   }
 }
