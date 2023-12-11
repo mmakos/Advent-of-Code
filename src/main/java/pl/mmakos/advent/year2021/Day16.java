070798931dddb8082dfb32d2a865f888441418e2
mmakos <mmakos.pl@gmail.com>
2023-12-11 21:29:41 +0100

    Added year 2021 day 16

--- src/main/java/pl/mmakos/advent/year2021/Day16.java
+++ src/main/java/pl/mmakos/advent/year2021/Day16.java
@@ -4,8 +4,10 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import pl.mmakos.advent.utils.Utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -13,47 +15,88 @@ import java.util.stream.Stream;
 public final class Day16 {
   @SuppressWarnings("java:S106")
   public static void main(String[] args) {
-    System.err.println("TASK 1: " + task1());
-    System.err.println("TASK 2: " + task2());
+    long[] result = parsePackets(input());
+    System.err.println("TASK 1: " + result[0]);
+    System.err.println("TASK 2: " + result[2]);
   }
 
-  private static int task1() {
-    return parsePackets(input());
-  }
-
-  private static int task2() {
-    return 0;
-  }
-
-  private static int parsePackets(boolean[] bits) {
-    int version = toInt(bits, 0, 3);
+  private static long[] parsePackets(boolean[] bits) {
+    long version = toInt(bits, 0, 3);
     int type = toInt(bits, 3, 3);
     if (type == 4) {
-      int end = 0;
-      while (bits[6 + end]) {
+      int end = 6;
+      long value = 0;
+      do {
+        value <<= 4;
+        value |= toInt(bits, 1 + end, 4);
         end += 5;
-      }
-      end += 5;
-      end += (4 - end % 4) % 4;
-      return version + bits.length > end ? parsePackets(Arrays.copyOfRange(bits, end, bits.length)) : 0;
+      } while (bits[end - 5]);
+      return new long[]{version, end, value};
     } else {
+      long[][] subPackets;
+      long end;
       if (bits[6]) {
-        throw new UnsupportedOperationException();
+        int packets = toInt(bits, 7, 11);
+        end = 18;
+        subPackets = subPackets(Arrays.copyOfRange(bits, 18, bits.length), packets);
       } else {
         int length = toInt(bits, 7, 15);
-        return version + parsePackets(Arrays.copyOfRange(bits, 22, 22 + length));
-//            + bits.length > 22 + length ? parsePackets(Arrays.copyOfRange(bits, 22 + length, bits.length)) : 0;
+        end = 22;
+        subPackets = subPackets(Arrays.copyOfRange(bits, 22, 22 + length), -1);
       }
+      end += subPackets[subPackets.length - 1][0];
+      long value = calculate(subPackets, type);
+      for (int i = 0; i < subPackets.length - 1; ++i) {
+        version += subPackets[i][0];
+      }
+      return new long[] {version, end, value};
     }
+  }
+
+  // Returns array:
+  // Packets (array: version, end, value)
+  // On the end one-element array with 'end'
+  private static long[][] subPackets(boolean[] bits, int packets) {
+    long end = 0;
+    List<long[]> subPackets = new ArrayList<>();
+    while (packets > 0 || (packets < 0 && end < bits.length)) {
+      long[] packet = parsePackets(Arrays.copyOfRange(bits, (int) end, bits.length));
+      subPackets.add(packet);
+      end += packet[1];
+      --packets;
+    }
+    subPackets.add(new long[]{end});
+    return subPackets.toArray(long[][]::new);
+  }
+
+  private static long calculate(long[][] packets, int type) {
+    long[] array = Arrays.stream(packets)
+            .limit(packets.length - 1L)
+            .mapToLong(i -> i[2])
+            .toArray();
+    return getCalculator(type).apply(array);
+  }
+
+  private static Function<long[], Long> getCalculator(int type) {
+    return switch (type) {
+      case 0 -> i -> Arrays.stream(i).sum();
+      case 1 -> i -> Arrays.stream(i).reduce(1, (i1, i2) -> i1 * i2);
+      case 2 -> i -> Arrays.stream(i).min().orElseThrow();
+      case 3 -> i -> Arrays.stream(i).max().orElseThrow();
+      case 5 -> i -> (long) (i[0] > i[1] ? 1 : 0);
+      case 6 -> i -> (long) (i[0] < i[1] ? 1 : 0);
+      case 7 -> i -> (long) (i[0] == i[1] ? 1 : 0);
+      default -> throw new IllegalStateException();
+    };
   }
 
   private static boolean[] input() {
     String string = Utils.read();
     List<Boolean> list = IntStream.range(0, string.length())
-        .mapToObj(i -> string.substring(i, i + 1))
-        .map(c -> Integer.parseInt(c, 16))
-        .flatMap(i -> Stream.of((i & 0b1000) > 0, (i & 0b100) > 0, (i & 0b10) > 0, (i & 0b1) > 0))
-        .toList();
+            .mapToObj(i -> string.substring(i, i + 1))
+            .map(c -> Integer.parseInt(c, 16))
+            .flatMap(i -> Stream.of((i & 0b1000) > 0, (i & 0b100) > 0, (i & 0b10) > 0, (i & 0b1) > 0))
+            .toList();
     return Utils.toPrimitiveBools(list);
   }
 
