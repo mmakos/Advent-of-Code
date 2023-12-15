f960ee7e6c0f97f19d3e972a47a825d583f8d42f
Michał Makoś <mmakos.pl@gmail.com>
2023-12-15 09:28:02 +0100

    Added year 2023 day 15

--- src/main/java/pl/mmakos/advent/year2023/Day15.java
+++ src/main/java/pl/mmakos/advent/year2023/Day15.java
@@ -1,7 +1,15 @@
 package pl.mmakos.advent.year2023;
 
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static pl.mmakos.advent.utils.Utils.strings;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public final class Day15 {
@@ -12,10 +20,57 @@ public final class Day15 {
   }
 
   private static int task1() {
-    return 0;
+    return strings(",")
+            .mapToInt(Day15::hashCode)
+            .sum();
   }
 
+  @SuppressWarnings("unchecked")
   private static int task2() {
-    return 0;
+    List<SI>[] boxes = IntStream.range(0, 256).mapToObj(i -> new ArrayList<>()).toArray(List[]::new);
+
+    strings(",")
+            .map(s -> s.split("[=\\-]"))
+            .forEach(s -> addOrRemoveLabel(s, boxes));
+
+    return IntStream.range(0, boxes.length)
+            .map(i -> IntStream.range(0, boxes[i].size())
+                    .map(j -> (i + 1) * (j + 1) * boxes[i].get(j).i)
+                    .sum())
+            .sum();
+  }
+
+  private static void addOrRemoveLabel(String[] s, List<SI>[] boxes) {
+    int hash = hashCode(s[0]);
+    int existingIdx = boxes[hash].indexOf(new SI(s[0], 0));
+
+    if (s.length == 2) {
+      SI si = new SI(s[0], Integer.parseInt(s[1]));
+      if (existingIdx >= 0) {
+        boxes[hash].set(existingIdx, si);
+      } else {
+        boxes[hash].add(si);
+      }
+    } else if (existingIdx >= 0) {
+      boxes[hash].remove(existingIdx);
+    }
+  }
+
+  private static int hashCode(String string) {
+    int hash = 0;
+    for (int i = 0; i < string.length(); ++i) {
+      hash += string.charAt(i);
+      hash *= 17;
+      hash %= 256;
+    }
+    return hash;
+  }
+
+  @EqualsAndHashCode
+  @RequiredArgsConstructor
+  private static class SI {
+    private final String s;
+    @EqualsAndHashCode.Exclude
+    private final int i;
   }
 }
