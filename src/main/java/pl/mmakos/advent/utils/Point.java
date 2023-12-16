87c4f8f5513bf441bc7c417288e50a9ee7b937a7
mmakos <mmakos.pl@gmail.com>
2023-12-16 11:44:58 +0100

    Added year 2023 day 16

--- src/main/java/pl/mmakos/advent/utils/Point.java
+++ src/main/java/pl/mmakos/advent/utils/Point.java
@@ -1,6 +1,23 @@
 package pl.mmakos.advent.utils;
 
-public record Point(int x, int y) {
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class Point {
+  private final int x;
+  private final int y;
+
+  public int x() {
+    return x;
+  }
+
+  public int y() {
+    return y;
+  }
+
   public Point left() {
     return new Point(x - 1, y);
   }
@@ -39,5 +56,21 @@ public record Point(int x, int y) {
 
   public boolean isAfter(Rect rect) {
     return x >= rect.x() + rect.width() || y >= rect.y() + rect.height();
+  }
+
+  public boolean validBounds(char[][] array) {
+    return validBounds(0, 0, array[0].length, array.length);
+  }
+
+  public boolean validBounds(boolean[][] array) {
+    return validBounds(0, 0, array[0].length, array.length);
+  }
+
+  public boolean validBounds(int[][] array) {
+    return validBounds(0, 0, array[0].length, array.length);
+  }
+
+  public boolean validBounds(int x, int y, int width, int height) {
+    return this.x >= x && this.y >= y && this.x < x + width && this.y < y + height;
   }
 }
