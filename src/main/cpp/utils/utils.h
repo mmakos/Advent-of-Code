340d391b6a06da51e4e46d1bef52ee8bdb6464a5
m.makos <michal.makos@pitradwar.com>
2024-11-30 20:10:15 +0100

    Added C++ templates for 2024 Advent of Code

---
+++ src/main/cpp/utils/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <string>
+#include <vector>
+
+namespace aoc {
+    std::vector<std::string> readInput(int year, int day);
+}
+
+#endif //UTILS_H
---
+++ src/main/cpp/year2024/day24.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#include <filesystem>
+#include <utils.h>
+
+constexpr int day = 24;
+constexpr int year = 2024;
+
+int solve1(const std::vector<std::string> &input) {
+  return 0; // TODO
+}
+
+int solve2(const std::vector<std::string> &input) {
+  return 0; // TODO
+}
+
+int main() {
+  const std::vector<std::string> input = aoc::readInput(year, day);
+
+  std::cout << "Advent of code " << year << ", day " << day << std::endl;
+  std::cout << "TASK 1: " << solve1(input) << std::endl;
+  std::cout << "TASK 2: " << solve2(input) << std::endl;
+}
