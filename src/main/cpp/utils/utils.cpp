fc877d34f3a0466227b3a951f4c24f1dcd1dbfcd
m.makos <michal.makos@pitradwar.com>
2024-12-01 00:52:24 +0100

    Added C++ solution of 1 task from 2023 just to test template, build etc.

--- src/main/cpp/utils/utils.cpp
+++ src/main/cpp/utils/utils.cpp
@@ -1,25 +1,57 @@
 #include "utils.h"
 
+#include <algorithm>
 #include <format>
 #include <fstream>
+#include <iterator>
+#include <sstream>
 
 namespace aoc {
-    std::vector<std::string> readInput(const int year, const int day) {
-        const auto filename = std::format("input/year{}/day{}.txt", year, day);
-        std::vector<std::string> lines;
-        std::ifstream file(filename);
+  std::vector<std::string> readInput(const int year, const int day) {
+    const auto filename = std::format("input/year{}/day{}.txt", year, day);
+    std::vector<std::string> lines;
+    std::ifstream file(filename);
 
-        if (!file.is_open()) {
-            throw std::runtime_error(std::format(
-                "Could not open input file for day {} year {}: {}", day, year, filename));
-        }
-
-        std::string line;
-        while (std::getline(file, line)) {
-            lines.push_back(line);
-        }
-
-        file.close();
-        return lines;
+    if (!file.is_open()) {
+      throw std::runtime_error(std::format(
+        "Could not open input file for day {} year {}: {}", day, year, filename));
     }
+
+    std::string line;
+    while (std::getline(file, line)) {
+      lines.push_back(line);
+    }
+
+    file.close();
+    return lines;
+  }
+
+  std::string join(std::vector<std::string> const &elements, const char *const delimiter) {
+    std::ostringstream os;
+    auto b = begin(elements);
+    const auto e = end(elements);
+
+    if (b != e) {
+      std::copy(b, prev(e), std::ostream_iterator<std::string>(os, delimiter));
+      b = prev(e);
+    }
+    if (b != e) {
+      os << *b;
+    }
+
+    return os.str();
+  }
+
+  std::vector<std::string> split(const std::string &input, char delimiter) {
+    std::vector<std::string> output;
+    for (auto cur = begin(input), beg = cur; ; ++cur) {
+      if (cur == end(input) || *cur == delimiter || !*cur) {
+        output.emplace(output.end(), beg, cur);
+        if (cur == end(input) || !*cur)
+          break;
+        beg = next(cur);
+      }
+    }
+    return output;
+  }
 }
--- src/main/cpp/utils/utils.h
+++ src/main/cpp/utils/utils.h
@@ -2,9 +2,22 @@
 #define UTILS_H
 #include <string>
 #include <vector>
+#include <regex>
 
 namespace aoc {
-    std::vector<std::string> readInput(int year, int day);
+  std::vector<std::string> readInput(int year, int day);
+
+  template<typename T>
+  int indexOf(const std::vector<T> &vec, const T &element) {
+    if (auto it = std::ranges::find(vec, element); it != vec.end()) {
+      return std::distance(vec.begin(), it);
+    }
+    return -1;
+  }
+
+  std::string join(std::vector<std::string> const &elements, const char *delimiter = "");
+
+  std::vector<std::string> split(const std::string &input, char delimiter);
 }
 
 #endif //UTILS_H
