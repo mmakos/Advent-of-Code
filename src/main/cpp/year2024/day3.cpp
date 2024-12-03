f4efc1ec0eddc7d45d445e22c3725c354f888bbb
Michał Makoś <mmakos@merlin.pho>
2024-12-03 10:40:52 +0100

    Added C++ day 3 year 2024

--- src/main/cpp/year2024/day3.cpp
+++ src/main/cpp/year2024/day3.cpp
@@ -6,12 +6,35 @@
 constexpr int day = 3;
 constexpr int year = 2024;
 
-int solve1(const std::vector<std::string> &input) {
-  return 0; // TODO
+const std::string mul = R"(mul\((\d{1,3}),(\d{1,3})\))";
+
+long long solve(const std::vector<std::string> &input, bool task2=false) {
+  std::string inputStr = aoc::join(input, "\n");
+  std::regex regex( task2 ? mul + R"(|do\(\)|don't\(\))" : mul);
+
+  auto endIt = std::sregex_iterator();
+  long long sum = 0;
+  bool enabled = true;
+  for (auto i = std::sregex_iterator(inputStr.begin(), inputStr.end(), regex); i != endIt; ++i) {
+    const std::smatch &match = *i;
+    if (match[0] == "don't()") {
+      enabled = false;
+    } else if (match[0] == "do()") {
+      enabled = true;
+    } else if (enabled) {
+      sum += stoi(match[1]) * stoi(match[2]);
+    }
+  }
+
+  return sum;
 }
 
-int solve2(const std::vector<std::string> &input) {
-  return 0; // TODO
+long long solve1(const std::vector<std::string> &input) {
+  return solve(input);
+}
+
+long long solve2(const std::vector<std::string> &input) {
+  return solve(input, true);
 }
 
 int main() {
