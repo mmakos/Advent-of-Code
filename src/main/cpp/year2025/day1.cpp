cceaebbec4aaad47d332ca20f2539cd08d958ded
Michał Makoś <michal.makos@pitradwar.com>
2025-12-01 13:54:55 +0100

    Added C++ day 1 year 2025

---
+++ src/main/cpp/year2025/day1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+#include <utils.h>
+
+constexpr int day = 1;
+constexpr int year = 2025;
+
+int solve1(const std::vector<std::string> &input) {
+  int dial = 50;
+  int password = 0;
+  for (const auto &line : input) {
+    dial += (line[0] == 'L' ? -1 : 1) * std::stoi(line.substr(1));
+    dial = (dial % 100 + 100) % 100;
+    if (dial == 0) ++password;
+  }
+  return password;
+}
+
+int solve2(const std::vector<std::string> &input) {
+  int dial = 50;
+  int password = 0;
+  for (const auto &line : input) {
+    const int rotation = (line[0] == 'L' ? -1 : 1) * std::stoi(line.substr(1));
+    if (rotation >= 0) {
+      password += (dial + rotation) / 100;
+    } else {
+      password += (100 - dial - rotation) / 100;
+      if (dial == 0) --password;
+    }
+    dial = ((dial + rotation) % 100 + 100) % 100;
+  }
+  return password;
+}
+
+int main() {
+  const std::vector<std::string> input = aoc::readInput<std::string>(year, day);
+
+  std::cout << "Advent of code " << year << ", day " << day << std::endl;
+  std::cout << "TASK 1: " << solve1(input) << std::endl;
+  std::cout << "TASK 2: " << solve2(input) << std::endl;
+}
